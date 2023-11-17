ec81927cd0a0817fad84a720f0313d7cae0e4b22
Haoge Zhou <88472118+hankge@users.noreply.github.com>
2023-11-17 00:18:20 -0800

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -32,6 +32,7 @@ void send_file_data(std::ifstream& file, int sockfd, sockaddr_in& addr)
   // Sending the 'END'
   std::strcpy(buffer, "END");
   sendto(sockfd, buffer, SIZE, 0, (struct sockaddr*)&addr, sizeof(addr));
+
   file.close();
 }
 int main()
@@ -39,7 +40,12 @@ int main()
      // Defining the IP and Port
     char* ip = "192.168.1.96";
     const int port = 2000;
-    
+
+    // Defining variables
+ 
+
+    // std::string filename = "client.bin";
+    // std::ifstream file(filename, std::ios::binary);
     // make socket for transmission
     int fd = socket(PF_INET, SOCK_DGRAM, 0);
 
@@ -62,7 +68,9 @@ int main()
       exit(1);
     }
     std::string filename ;
+    int num = 1;
     while (std::getline(file,filename)){
+
       filename = "00_MP_TXT/"+filename + ".txt";
       std::ifstream inFile(filename );
 
@@ -71,10 +79,32 @@ int main()
       continue;
     }
     std::cerr << "sending" << filename << std::endl;
+    std::cerr << "number of files " << num << std::endl;
+    num++;
     send_file_data(inFile, fd, saddr);
     inFile.close();
     sleep(1);
     }
+
+    // int num = 0;
+
+
+    // Communication
+    // while (1)
+    // {
+    //     char buf[128];
+    //     sprintf(buf, "hello ,i am client %d\n", num++);
+
+    //     //send
+    //     sendto(fd, buf, strlen(buf) + 1,0,(struct sockaddr*)&saddr,sizeof(saddr));
+
+    //     //receive
+    //     int num = recvfrom(fd, buf, sizeof(buf), 0, NULL, NULL);
+
+    //     printf("server say:%s\n", buf);
+
+    //     sleep(1);
+    // }
     
     file.close();
     close(fd);
